29cfef27464c8492dd4a065057b778fe66698123
i-hidaka <itsuki.hidaka@rakus-partners.co.jp>
2023-08-25 12:06:43 +0900

    カラムを追加

---
+++ database/migrations/2023_08_25_114707_add_title_to_contact_forms_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddTitleToContactFormsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('contact_forms', function (Blueprint $table) {
+            $table->string("title", 50)->after("name"); // nameの後の列にtitleを追加
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('contact_forms', function (Blueprint $table) {
+            $table->dropColumn("title"); // up()の内容の削除　ロールバック（取り消し）したいときに必要
+        });
+    }
+}
